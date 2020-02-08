f8338ca779c3373ab805b1de94ce11e6b47d4694
Johan-S <johstraat@gmail.com>
2020-02-08 14:12:17 +0100

    Added editor script to automatically copy layouts to builds.

---
+++ Assets/Editor/CopyLayouts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+public class CopyLayouts : IPostprocessBuildWithReport
+{
+    public int callbackOrder => 10;
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        var out_path = Path.GetDirectoryName(report.summary.outputPath);
+        var layout_path = out_path + "/MapNuke_Data/Layouts";
+        Directory.CreateDirectory(layout_path);
+        foreach (var s in Directory.GetFiles("Assets/Layouts", "*.xml"))
+        {
+            if (s.EndsWith(".meta")) continue;
+            File.Copy(s, layout_path + '/' + Path.GetFileName(s));
+        }
+    }
+}
