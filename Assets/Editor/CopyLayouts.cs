3d9be645febca721a4ea37ad7098ecedfb91589a
Steven Kessler <stvnksslr@gmail.com>
2020-09-12 11:04:11 -0700

    chore(cleanup): code cleanup and refactor to streamline methods a bit so there is less duplicated code

--- Assets/Editor/CopyLayouts.cs
+++ Assets/Editor/CopyLayouts.cs
@@ -15,8 +15,8 @@ public class CopyLayouts : IPostprocessBuildWithReport
         if (report.summary.platform == BuildTarget.StandaloneOSX)
         {
             Debug.Log("This is a standalone macos build");
-            MacosFetchNations(report);
-            MacosFetchLayout(report);
+            MacosPackageAssets(report, "Layouts");
+            MacosPackageAssets(report, "Nations");
         }
         else
         {
@@ -38,28 +38,16 @@ public class CopyLayouts : IPostprocessBuildWithReport
     }
 
 
-    // These two methods exist because there does not seem to be an easy way to pull files specifically into the .app 
-    private static void MacosFetchNations(BuildReport report)
+    // This logic fork exist because there does not seem to be an easy way to pull files specifically into the .app 
+    private static void MacosPackageAssets(BuildReport report, string asset)
     {
         var outPath = Path.GetDirectoryName(report.summary.outputPath);
-        var layoutPath = $"{outPath}/MapNuke.app/Contents/Nations";
-        Directory.CreateDirectory(layoutPath);
-        foreach (var s in Directory.GetFiles("Assets/Nations", "*.xml"))
+        var assetPath = $"{outPath}/MapNuke.app/Contents/{asset}";
+        Directory.CreateDirectory(assetPath);
+        foreach (var s in Directory.GetFiles($"Assets/{asset}", "*.xml"))
         {
             if (s.EndsWith(".meta")) continue;
-            File.Copy(s, $"{layoutPath}/{Path.GetFileName(s)}");
-        }
-    }
-
-    private static void MacosFetchLayout(BuildReport report)
-    {
-        var outPath = Path.GetDirectoryName(report.summary.outputPath);
-        var layoutPath = $"{outPath}/MapNuke.app/Contents/Layouts";
-        Directory.CreateDirectory(layoutPath);
-        foreach (var s in Directory.GetFiles("Assets/Layouts", "*.xml"))
-        {
-            if (s.EndsWith(".meta")) continue;
-            File.Copy(s, $"{layoutPath}/{Path.GetFileName(s)}");
+            File.Copy(s, $"{assetPath}/{Path.GetFileName(s)}");
         }
     }
 }
