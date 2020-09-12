18766eb16c5bfb09783c6d60328494d000434019
Steven Kessler <stvnksslr@gmail.com>
2020-09-11 17:13:32 -0700

    feat(macos build fixes): layouts and contents folders are not getting packaged inside the MapNuke.app folder

--- Assets/Editor/CopyLayouts.cs
+++ Assets/Editor/CopyLayouts.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 using System.IO;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 
 public class CopyLayouts : IPostprocessBuildWithReport
 {
@@ -11,13 +12,54 @@ public class CopyLayouts : IPostprocessBuildWithReport
 
     public void OnPostprocessBuild(BuildReport report)
     {
-        var out_path = Path.GetDirectoryName(report.summary.outputPath);
-        var layout_path = out_path + "/MapNuke_Data/Layouts";
-        Directory.CreateDirectory(layout_path);
+        if (report.summary.platform == BuildTarget.StandaloneOSX)
+        {
+            Debug.Log("This is a standalone macos build");
+            MacosFetchNations(report);
+            MacosFetchLayout(report);
+        }
+        else
+        {
+            Debug.Log("This is a standalone linux/windows build");
+            FetchLayout(report);
+        }
+    }
+
+    private static void FetchLayout(BuildReport report)
+    {
+        var outPath = Path.GetDirectoryName(report.summary.outputPath);
+        var layoutPath = $"{outPath}/MapNuke_Data/Layouts";
+        Directory.CreateDirectory(layoutPath);
         foreach (var s in Directory.GetFiles("Assets/Layouts", "*.xml"))
         {
             if (s.EndsWith(".meta")) continue;
-            File.Copy(s, layout_path + '/' + Path.GetFileName(s));
+            File.Copy(s, $"{layoutPath}/{Path.GetFileName(s)}");
+        }
+    }
+
+
+    // These two methods exist because there does not seem to be an easy way to pull files specifically into the .app 
+    private static void MacosFetchNations(BuildReport report)
+    {
+        var outPath = Path.GetDirectoryName(report.summary.outputPath);
+        var layoutPath = $"{outPath}/MapNuke.app/Contents/Nations";
+        Directory.CreateDirectory(layoutPath);
+        foreach (var s in Directory.GetFiles("Assets/Nations", "*.xml"))
+        {
+            if (s.EndsWith(".meta")) continue;
+            File.Copy(s, $"{layoutPath}/{Path.GetFileName(s)}");
+        }
+    }
+
+    private static void MacosFetchLayout(BuildReport report)
+    {
+        var outPath = Path.GetDirectoryName(report.summary.outputPath);
+        var layoutPath = $"{outPath}/MapNuke.app/Contents/Layouts";
+        Directory.CreateDirectory(layoutPath);
+        foreach (var s in Directory.GetFiles("Assets/Layouts", "*.xml"))
+        {
+            if (s.EndsWith(".meta")) continue;
+            File.Copy(s, $"{layoutPath}/{Path.GetFileName(s)}");
         }
     }
 }
